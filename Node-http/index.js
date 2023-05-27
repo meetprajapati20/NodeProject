64e7b3175c69afe864e662a6873a768c88f33e37
meetprajapati20 <meetprajapati20@gnu.ac.in>
2023-05-27 13:49:19 +0530

    final

--- Node-http/index.js
+++ Node-http/index.js
@@ -1,14 +1,42 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    console.log(req.headers);
+    // console.log(req.headers);
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type','text/html');
+    // res.end('<html><body>Hii Meet</body></html>');
+    console.log("Requested URL : "+req.url+" By Method : ",req.method);
+    if (req.method == "GET"){
+        var fileURL;
+        if(req.url == "/") fileURL="/index.html";
+        else fileURL = req.url;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body>Hii Meet</body></html>');
+        var filePATH = path.resolve('./public'+fileURL);
+        const extFile = path.extname(filePATH);
+        if (extFile == ".html"){
+            fs.exists(filePATH,(exists)=> {
+                if (!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type',"text/html");
+                    res.end('<html><body>[ERROR] 404 File :'+fileURL+' Not Found at '+filePATH+'</body></html>');
+                    return ;
+                } 
+                res.statusCode = 200;
+                res.setHeader('Comtent-Type',"text/html");
+                fs.createReadStream(filePATH).pipe(res);
+            }) ;
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type',"text/html");
+            res.end('<html><body>[ERROR] 404 File :'+fileURL+' Not HTML File</body></html>');
+            return ; 
+        }
+    }
 });
 
 server.listen(port,hostname,() => {
